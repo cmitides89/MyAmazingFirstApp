bbe85703954ab6c39ee6215699344206cf176e13
Constantin <mitides.constantin@gmail.com>
2013-03-25 15:51:53 -0400

    added everything else....sorry

--- my_amazing_web_app.rb
+++ my_amazing_web_app.rb
@@ -11,22 +11,28 @@ get '/' do
 end
 
 post '/submit' do
-  @message = "Someone thought your cupcakes were #{params[:answer]}"
-  
-  # Change these to match your Twilio account settings 
-  @account_sid = "ACf8e98f3fd96dbe29d0588dfcfa1b673d"
-  @auth_token = "73d0ca498d29a3143ce99c8cab050d09"
-  
-  # Set up a client to talk to the Twilio REST API
-  @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+  if params[:tc] == "1" 
+    @message = "#{params[:name]} thought your cupcakes were #{params[:answer]}. Thanks for voting #{params[:name]}!"
     
-  @account = @client.account
-  @sms = @account.sms.messages.create({
-    :from => '+16464614467', 
-    :to => '+19174968239',
-    :body => @message
-  })
-  
-  flash[:notice] = "SMS sent: #{@message}"
+    # Change these to match your Twilio account settings 
+    @account_sid = "ACf8e98f3fd96dbe29d0588dfcfa1b673d"
+    @auth_token = "73d0ca498d29a3143ce99c8cab050d09"
+    
+    # Set up a client to talk to the Twilio REST API
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+      
+    @account = @client.account
+    @sms = @account.sms.messages.create({
+      :from => '+16464614467', 
+      :to => '+19174968239',
+      :body => @message
+    })
+    
+    flash[:notice] = "SMS sent: #{@message}"
+    redirect '/'
+  else
+  @message2 = "You did not fill out the whole form"
+  flash[:notice] = "#{@message2}"
   redirect '/'
+  end
 end
